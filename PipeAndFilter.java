f72a3c740358847ec6d5937b38c9266bdee08d7a
SardarKamran <145672543+SardarKamran@users.noreply.github.com>
2025-03-29 23:04:42 +0500

    Create PipeAndFilter.java
    lab3 done

---
+++ PipeAndFilter.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PipeAndFilter {
+    public static void main(String[] args) {
+
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Function<List<Integer>, List<Integer>>> filters = new ArrayList<>();
+
+        filters.add(PipeAndFilter::filterEvenNumber);
+        filters.add(PipeAndFilter::squareNumbers);
+        filters.add(PipeAndFilter::filterNumberGreatherthenten);
+        filters.add(PipeAndFilter::filterPrime);
+
+        // process the input throught the Pipeline
+        List<Integer> result = processPipeLine(input, filters);
+
+        System.out.println(result);
+    }
+
+    // process the input through the pipeline of filters
+    private static List<Integer> processPipeLine(List<Integer> input,
+            List<Function<List<Integer>, List<Integer>>> filters) {
+        List<Integer> output = input;
+
+        for (Function<List<Integer>, List<Integer>> filter : filters) {
+            output = filter.apply(output);
+        }
+        return output;
+    }
+
+    // filter for even number
+    private static List<Integer> filterEvenNumber(List<Integer> input) {
+        return input.stream()
+                .filter(n -> n % 2 == 0).collect(Collectors.toList());
+    }
+
+    // filter for square root
+    private static List<Integer> squareNumbers(List<Integer> input) {
+        return input.stream()
+                .map(n -> n * n).collect(Collectors.toList());
+    }
+
+    // filter Greater then 10
+    private static List<Integer> filterNumberGreatherthenten(List<Integer> input) {
+        return input.stream()
+                .filter(n -> n > 10).collect(Collectors.toList());
+    }
+
+    private static List<Integer> filterPrime(List<Integer> input) {
+        return input.stream()
+                .filter(PipeAndFilter::isPrime)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+}
